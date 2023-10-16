29a69e50363b82059a94c97e65a8d7add6f732f1
test <ten13zin@gmail.com>
2023-10-16 13:02:57 +0530

    fix(mt-page): add lang switch

--- app/routes/tools.mt.tsx
+++ app/routes/tools.mt.tsx
@@ -5,13 +5,31 @@ import {
   FaRegThumbsUp,
 } from "react-icons/fa6/index.js";
 
+import { useState } from "react";
+
+const langLabels = {
+  bo: "Tibetan",
+  en: "English",
+};
+
 export default function Index() {
+  const [sourceLang, setSourceLang] = useState("bo");
+  const [targetLang, setTargetLang] = useState("en");
+
+  const handleLangSwitch = () => {
+    const temp = sourceLang;
+    setSourceLang(targetLang);
+    setTargetLang(temp);
+  };
+
   return (
     <main className="m-auto w-11/12 md:w-4/5">
       <h1>MT Page</h1>
-      <div className="flex gap-3">
+      <div className="flex items-center gap-1">
         <Card className="w-1/2">
-          <h3 className="text-center font-bold text-gray-800">Tibetan</h3>
+          <h3 className="text-center font-bold text-gray-800">
+            {langLabels[sourceLang]}
+          </h3>
           <Textarea
             placeholder="Enter your text here"
             className="w-full h-48"
@@ -26,10 +44,18 @@ export default function Index() {
             </Button>
           </div>
         </Card>
-        <FaArrowRightArrowLeft className="lg:mt-6" size="20px" />
+        <Button
+          className="self-start lg:mt-4"
+          color="transparent"
+          onClick={handleLangSwitch}
+        >
+          <FaArrowRightArrowLeft size="20px" className="text-gray-400" />
+        </Button>
         <Card className="w-1/2">
-          <h3 className="text-center font-bold text-gray-900">English </h3>
-          <Textarea className="w-full h-48" disabled />
+          <h3 className="text-center font-bold text-gray-900">
+            {langLabels[targetLang]}
+          </h3>
+          <div className="w-full h-48"></div>
           <div className="flex justify-end">
             <Button color="white">
               <FaRegThumbsUp color="gray" />
