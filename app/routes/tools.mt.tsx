e1ae9b14c7be5a7db6bd0dbf122d9283f038306f
test <ten13zin@gmail.com>
2023-10-16 19:13:12 +0530

    fix(mt-page): tibetan input as list

--- app/routes/tools.mt.tsx
+++ app/routes/tools.mt.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Textarea } from "flowbite-react";
+import { Button, Card, Label, Radio, Textarea } from "flowbite-react";
 import {
   FaArrowRightArrowLeft,
   FaRegThumbsDown,
@@ -13,6 +13,12 @@ const langLabels = {
 };
 
 const charLimit = 500;
+
+const boTexts = [
+  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit fuga obcaecati inventore recusandae natus, repellendus libero mollitia! Pariatur error a temporibus dicta ex, officia dolorem distinctio dignissimos similique! Laborum hic fuga atque voluptatibus veniam quibusdam nisi ipsa fugiat. In nulla iusto architecto quasi, doloribus odit laborum soluta nesciunt assumenda eos",
+  "Pariatur error a temporibus dicta ex, officia dolorem distinctio dignissimos similique! Laborum hic fuga atque voluptatibus veniam quibusdam nisi ipsa fugiat. In nulla iusto architecto quasi, doloribus odit laborum soluta nesciunt assumenda eos",
+  "Fugit fuga obcaecati inventore recusandae natus, repellendus libero mollitia! Pariatur error a temporibus dicta ex, officia dolorem distinctio dignissimos similique! Laborum hic fuga atque voluptatibus veniam quibusdam nisi ipsa fugiat. In nulla iusto architecto quasi, doloribus odit laborum soluta nesciunt assumenda eos",
+];
 
 export default function Index() {
   const [sourceLang, setSourceLang] = useState("bo");
@@ -32,27 +38,62 @@ export default function Index() {
   };
 
   return (
-    <main className="m-auto w-11/12 md:w-4/5">
-      <h1>MT Page</h1>
-      <div className="flex items-center gap-1 ">
+    <main className="mx-auto w-11/12 md:w-4/5">
+      <h1 className="mb-5 text-xl text-center">Machine Translation</h1>
+      <div className="flex items-strech gap-1 ">
         <Card className="w-1/2">
           <h3 className="text-center font-bold text-gray-800">
             {langLabels[sourceLang]}
           </h3>
-          <Textarea
-            placeholder="Enter your text here"
-            className="w-full h-48"
-            required
-            value={sourceText}
-            onChange={handleOnChange}
-          />
+          {sourceLang === "en" ? (
+            <div className="w-full h-[60vh]">
+              <Textarea
+                placeholder="Enter your text here"
+                className="w-full h-full"
+                required
+                value={sourceText}
+                onChange={handleOnChange}
+              />
+            </div>
+          ) : (
+            <div className="w-full h-[60vh] overflow-auto">
+              <fieldset className="w-full flex" id="radio">
+                <legend className="mb-4 text-gray-400">
+                  Choose a text to translate from:
+                </legend>
+                <div className="flex flex-col gap-4">
+                  {boTexts.map((text, index) => (
+                    <div
+                      className="p-2 flex w-full items-center gap-2 border rounded-md"
+                      key={index}
+                    >
+                      <Radio
+                        id={"eg" + index}
+                        value={text}
+                        name="texts"
+                        defaultChecked={index === 0}
+                      />
+                      <Label htmlFor={"eg" + index}>{text}</Label>
+                    </div>
+                  ))}
+                </div>
+              </fieldset>
+              <div className="mt-10">
+                <p className="text-gray-600">
+                  Join the waitlist to Tibetan input
+                </p>
+              </div>
+            </div>
+          )}
           <div className="flex justify-between items-center">
             <Button pill color="gray" size="xs">
               Reset
             </Button>
-            <div className="text-gray-400 text-xs">
-              {charCount} / {charLimit}
-            </div>
+            {sourceLang === "en" && (
+              <div className="text-gray-400 text-xs">
+                {charCount} / {charLimit}
+              </div>
+            )}
             <Button pill color="success" size="xs">
               Submit
             </Button>
@@ -70,7 +111,7 @@ export default function Index() {
           <h3 className="text-center font-bold text-gray-900">
             {langLabels[targetLang]}
           </h3>
-          <div className="w-full h-48"></div>
+          <div className="w-full h-[60vh]"></div>
           <div className="flex justify-end">
             <Button color="white">
               <FaRegThumbsUp color="gray" />
