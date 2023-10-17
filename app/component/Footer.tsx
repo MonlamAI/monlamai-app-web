33fe5dc20e1a07325ff7e10be8694be8eca7e554
test <ten13zin@gmail.com>
2023-10-17 16:35:07 +0530

    fix: make footer ligher in color

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -2,13 +2,13 @@ function Footer() {
   return (
     <footer className="p-4">
       <div className="w-full mx-auto max-w-screen-xl p-4 flex flex-col md:flex-row items-center md:justify-between">
-        <span className="text-sm text-gray-700 sm:text-center">
+        <span className="text-sm text-gray-400 sm:text-center">
           <a href="#" className="hover:underline">
             © Monlam IT 2023
           </a>
           . All Rights Reserved.
         </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-700 sm:mt-0">
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-400 sm:mt-0">
           <li>
             <a href="#" className="mr-4 hover:underline md:mr-6 ">
               <i className="fab fa-facebook-square"></i>
