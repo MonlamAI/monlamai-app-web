f54d1c58b2b022fed5b26d0b1157522d5ee83f8b
test <ten13zin@gmail.com>
2023-10-18 15:03:42 +0530

    fix(mt): increase page title size

--- app/routes/tools.mt.tsx
+++ app/routes/tools.mt.tsx
@@ -141,7 +141,9 @@ export default function Index() {
 
   return (
     <main className="mx-auto w-full md:w-11/12 lg:4/5">
-      <h1 className="mb-10 text-3xl text-center">Monlam Translation</h1>
+      <h1 className="mb-10 text-4xl lg:text-5xl text-center">
+        Monlam Translation
+      </h1>
       <div className="flex flex-col md:flex-row items-strech md:gap-1">
         <Card className="w-1/2">
           <h3 className="text-lg text-gray-500">{langLabels[sourceLang]}</h3>
