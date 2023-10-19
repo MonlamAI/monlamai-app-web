1b3dac82d958a3475f9cbdb8d28aa79cfba8508b
test <ten13zin@gmail.com>
2023-10-19 12:02:22 +0530

    fix(ocr): render selected image

--- app/routes/tools.ocr.tsx
+++ app/routes/tools.ocr.tsx
@@ -2,7 +2,20 @@ import type { ActionFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Form, useActionData, useNavigation } from "@remix-run/react";
 import { Button, Card, FileInput, Label, Spinner } from "flowbite-react";
+import parse from "html-react-parser";
+import { useState } from "react";
 import { FaRegThumbsDown, FaRegThumbsUp } from "react-icons/fa6/index.js";
+
+function toParaTags(textLines: string[]) {
+  return textLines
+    .map((line) => {
+      if (line.trim() === "") {
+        return "</p><p>";
+      }
+      return "<p>" + line + "</p>";
+    })
+    .join(" ");
+}
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
@@ -22,7 +35,7 @@ export async function action({ request }: ActionFunctionArgs) {
 
     const data = await response.json();
     return json({
-      text: data.output,
+      text: toParaTags(data.output),
     });
   } catch (error) {
     console.error("There was a problem with the fetch operation:", error);
@@ -30,9 +43,18 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function Index() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
   const data = useActionData<typeof action>();
   const navigation = useNavigation();
   const isActionSubmission = navigation.state == "submitting";
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
 
   return (
     <main className="mx-auto w-11/12 lg:4/5">
@@ -43,7 +65,7 @@ export default function Index() {
       <div className="mt-1 flex flex-col md:flex-row items-strech gap-5">
         <Card className="md:w-1/2">
           <Form method="post" encType="multipart/form-data">
-            <div className="w-full min-h-[50vh] flex items-center justify-center">
+            <div className="w-full min-h-[50vh] flex flex-col items-center justify-center gap-5">
               <div>
                 <div className="mb-2 block">
                   <Label htmlFor="file" value="Upload your image here" />
@@ -53,8 +75,18 @@ export default function Index() {
                   id="file"
                   name="image"
                   accept="image/png, image/jpeg, image/jpg"
+                  required
+                  onChange={handleFileChange}
                 />
               </div>
+
+              {selectedFile && (
+                <img
+                  src={URL.createObjectURL(selectedFile)}
+                  alt="selected file"
+                  style={{ maxWidth: "100%", maxHeight: "300px" }}
+                />
+              )}
             </div>
             <div className="flex justify-end">
               <Button
@@ -72,11 +104,11 @@ export default function Index() {
           <div className="w-full h-[50vh] p-3 text-black bg-slate-50 rounded-lg overflow-auto">
             {isActionSubmission ? (
               <div className="h-full flex justify-center items-center">
-                <Spinner />
+                <Spinner size="lg" />
               </div>
             ) : (
-              <div className="text-lg font-monlam tracking-wide leading-loose">
-                {data && data.text}
+              <div className="text-lg font-monlam tracking-wide leading-loose overflow-auto">
+                {data && parse(data.text)}
               </div>
             )}
           </div>
