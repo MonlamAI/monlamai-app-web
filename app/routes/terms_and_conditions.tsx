7d8edb968ccf19fd03df188b2ac9fd088ba8ccd6
test <ten13zin@gmail.com>
2023-10-20 11:48:15 +0530

    fix: add terms and conditions page

---
+++ app/routes/terms_and_conditions.tsx
@@ -0,0 +1,44 @@
+export default function Index() {
+  return (
+    <main className="min-h-screen bg-gray-100 flex flex-col justify-center md:py-12 sm:px-6 lg:px-8">
+      <div className="w-full lg:max-w-screen-md m-auto space-y-8 p-6 bg-white rounded-lg shadow-lg">
+        <h1 className="text-3xl font-semibold text-center">
+          Terms and Conditions
+        </h1>
+        <p>
+          Welcome to MonlamAI. Please read these terms and conditions carefully
+          before using our website and services. By accessing or using the
+          service, you agree to be bound by these Terms. If you disagree with
+          any part of the terms, then you may not access the service.
+        </p>
+        <h2 className="text-xl font-semibold">1. Introduction</h2>
+        <p>
+          These Terms and Conditions govern your use of the MonlamAI website and
+          any associated services (the "Service") offered by MonlamAI. MonlamAI
+          reserves the right to update, change, or replace any part of these
+          Terms and Conditions by posting updates and changes to our website. It
+          is your responsibility to check our website periodically for changes.
+          Your continued use of or access to our website or the Service
+          following the posting of any changes to these Terms and Conditions
+          constitutes acceptance of those changes.
+        </p>
+        <h2 className="text-xl font-semibold">2. Data Collection and Use</h2>
+        <p>
+          By using MonlamAI's services, you agree that MonlamAI is entitled to
+          collect and store data and information you submit or make available to
+          our machine learning models. The nature of the data collected is
+          explicit to the functioning of machine learning technologies,
+          requiring inputs for improving and further developing the capabilities
+          of our AI models.
+        </p>
+        <h2 className="text-xl font-semibold">3. Data Security</h2>
+        <p>
+          MonlamAI takes the security of your data very seriously and will use
+          appropriate technical and organisational measures to protect your data
+          from unauthorised disclosure or access.
+        </p>
+        <p className="text-center">We thank you for choosing MonlamAI.</p>
+      </div>
+    </main>
+  );
+}
