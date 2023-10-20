254f6eb74759321eec5870bf0067b91ff1c19eb6
TENZIN TAMDIN <tenzintamdin@TENZINs-MacBook-Air.local>
2023-10-20 18:25:15 +0530

    fix: tools styling

--- app/component/Tools.tsx
+++ app/component/Tools.tsx
@@ -7,23 +7,18 @@ type ModalType = {
   name: string;
   desc: string;
   link: string;
+  color: string;
 };
 
 type EachProps = {
   model: ModalType;
   index: number;
-  checkAuth: (link: string) => void;
   navigateTo: (link: string) => void;
 };
 
 function Tools() {
   const { user } = useLoaderData();
   let navigater = useNavigate();
-
-  function checkAuth(link: string) {
-    console.log("user", user);
-    navigater("/tools/" + link);
-  }
 
   function navigateTo(link: string) {
     navigater("/tools/" + link);
@@ -33,26 +28,30 @@ function Tools() {
     {
       icon: "fa fa-solid fa-globe",
       name: "ཡིག་སྒྱུར་རིག་ནུས།",
-      desc: "དབང་རྩའི་འཕྲུལ་རིག་གི་ཡིག་སྒྱུར་རིག་ནུས།",
+      desc: "དབྱིན་བོད་ཕན་ཚུན་ཡིག་སྒྱུར་བྱེད་ཐུབ།",
       link: "mt",
+      color: "#ff006a",
     },
     {
       icon: "fas fa-volume-up",
       name: "ཀློག་འདོན་རིག་ནུས།",
-      desc: "ཡེ་གེ་ཀློག་འདོན་གྱིས་འགྲོ་བ་མིའི་ངག་གི་འགན་སྒྲུབ་ཐུབ།",
+      desc: "བོད་ཡིག་གཏག་མ་ཀློག་འདོན་བྱེད་ཐུབ།",
       link: "tts",
+      color: "#00AAFF",
     },
     {
       icon: "fas fa-assistive-listening-systems",
       name: "སྒྲ་འཛིན་རིག་ནུས།",
-      desc: "རིག་ནུས་འདིའི་བོད་སྐད་གོ་ཐུབ་པའི་ཁྱད་ཆོས་ལྡན།",
+      desc: "བོད་སྐད་ཡིག་འབེབས་བྱེད་ཐུབ།",
       link: "stt",
+      color: "#FF0000",
     },
     {
       icon: "fas fa-file-alt",
-      name: "གཟུགས་འཛིན་རིག་ནུས།",
-      desc: "པར་རིས་ཡིག་གཟུགས་འདྲ་མིན་ངོས་འཛིན་བྱེད་ཐུབ།",
+      name: "ཡིག་འཛིན་རིག་ནུས།",
+      desc: "པར་རིས་ནང་གི་བོད་ཡིག་ཡིག་འབེབས་བྱེད་ཐུབ།",
       link: "ocr",
+      color: "#9933FF",
     },
   ];
 
@@ -60,9 +59,8 @@ function Tools() {
     <main>
       <div className="py-[100px]" id="skills">
         <div className="text-center  max-w-[1140px] mx-auto">
-          <h2 className="text-[2rem] mb-2">སྨོན་མ་རིག་ནུས་ཞབས་ཞུའི་མ་ལག</h2>
           <p className="mb-8">
-            གཤམ་གསལ་ཚོད་ལྟའི་མ་ལག་ཁག་ལ་ཁྱེད་རང་མཉམ་ཞུགས་བྱ་ཆོག
+            གཤམ་གསལ་ཚོད་ལྟའི་རིག་ནུས་ཁག་ཁྱེད་ཀྱིས་མཉམ་སྤྱོད་བྱ་ཆོག།
           </p>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-4 mx-10">
             {models.map((model, index) => (
@@ -70,7 +68,6 @@ function Tools() {
                 key={model.name}
                 model={model}
                 index={index}
-                checkAuth={checkAuth}
                 navigateTo={navigateTo}
               />
             ))}
@@ -81,12 +78,14 @@ function Tools() {
   );
 }
 
-function EachModel({ model, index, checkAuth, navigateTo }: EachProps) {
-  const { name, desc, icon, link } = model;
+function EachModel({ model, index, navigateTo }: EachProps) {
+  const { name, desc, icon, link, color } = model;
 
   return (
-    <Card className="shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
-      <i className={icon} style={{ fontSize: 24 }}></i>
+    <Card className="shadow-lg hover:border-blue-600 hover:border-2">
+      <div>
+        <i className={icon} style={{ fontSize: 35, color: color }}></i>
+      </div>
       <h2 className="text-xl">{name}</h2>
       <p className="text-gray-400">{desc}</p>
       <button
