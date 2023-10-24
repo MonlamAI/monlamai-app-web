56806f41e7206530b56268c606254f8389785830
kunsang <tenkus47@gmail.com>
2023-10-24 10:48:59 +0530

    fix newline error on mt

--- app/component/TypingText.tsx
+++ app/component/TypingText.tsx
@@ -4,22 +4,29 @@ import { motion, useAnimation } from "framer-motion";
 const TypingAnimation = ({ text }) => {
   const [displayText, setDisplayText] = useState("");
   const controls = useAnimation();
-
   useEffect(() => {
     const animateText = async () => {
-      setDisplayText("");
+      setDisplayText(""); // Start with an empty string
       for (let i = 0; i <= text.length; i++) {
-        setDisplayText(text.slice(0, i));
+        setDisplayText(text.slice(0, i)); // Replace '\\n' with '\n'
         await controls.start({ opacity: 1 });
         await controls.start({ opacity: 0 });
-        await new Promise((resolve) => setTimeout(resolve, 100));
+        await new Promise((resolve) => setTimeout(resolve, 50));
       }
     };
 
     animateText();
   }, [text, controls]);
 
-  return <motion.div>{displayText}</motion.div>;
+  return (
+    <div>
+      <motion.div
+        dangerouslySetInnerHTML={{
+          __html: displayText?.replaceAll("\n", "<br/>"),
+        }}
+      ></motion.div>
+    </div>
+  );
 };
 
 export default TypingAnimation;
