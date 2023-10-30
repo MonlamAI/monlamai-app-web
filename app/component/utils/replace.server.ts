2467258f51cefc0a6f1242fe0036762f650bf78e
Monlam IT <mlbodyig@gmail.com>
2023-10-30 14:07:36 +0530

    Update replace.server.ts

--- app/component/utils/replace.server.ts
+++ app/component/utils/replace.server.ts
@@ -1806,6 +1806,7 @@ export function en_bo_tibetan_replaces(text: string) {
   result = result.replaceAll("ངེས་གཏན་བྲལ་བའི་", "");
   result = result.replaceAll("༡༩༥༩ ལོའི་", "");
   result = result.replaceAll("<unk>", "");
+  
 
   result = result.replaceAll(
     "༸ཏཱ་ལའི་བླ་མའི་དགོངས་པ་རྫོགས་ཚུལ་",
