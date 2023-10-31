9c957dd4a7bbfac29fadb22499edc30770a41cbf
kunsang <tenkus47@gmail.com>
2023-10-31 14:24:50 +0530

    add audio compression

--- app/component/utils/audioGain.ts
+++ app/component/utils/audioGain.ts
@@ -12,6 +12,16 @@ export function amplifyMedia(mediaElem, multiplier) {
         return result.gain.gain.value;
       },
     };
+
+  let compressor = context.createDynamicsCompressor();
+
+  // You can tweak these values as per your requirement
+  compressor.threshold.value = -50;
+  compressor.knee.value = 40;
+  compressor.ratio.value = 12;
+  compressor.attack.value = 0;
+  compressor.release.value = 0.25;
+
   result.source.connect(result.gain);
   result.gain.connect(context.destination);
   result.amplify(multiplier);
