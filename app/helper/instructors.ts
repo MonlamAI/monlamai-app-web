c248c07c8c9e859d4932043e06f327e4ae65ce71
TENZIN TAMDIN <tenzintamdin@TENZINs-MacBook-Air.local>
2023-11-01 12:33:17 +0530

    add instructor imgs and data

---
+++ app/helper/instructors.ts
@@ -0,0 +1,58 @@
+export const instructors = [
+  {
+    id: 1,
+    name: "DR. KURT KEUTZER",
+    image: "",
+    description: "Professor Emeritus, UC Berkely",
+  },
+  {
+    id: 2,
+    name: "Geshe Thupten Jinpa, Phd",
+    image: "",
+    description: "Founder, Compassion Institute Various",
+  },
+  {
+    id: 3,
+    name: "Geshe Dorji Damdul",
+    image: "",
+    description: "Director, Tibet House, New Delhi",
+  },
+  {
+    id: 4,
+    name: "Sebastian Nehrdich",
+    image: "",
+    description: "Researcher, UC Berkeley",
+  },
+  {
+    id: 5,
+    name: "Nicolas Tournadre",
+    image: "",
+    description: "Professor, Dept. of linguistics, Aix-Marseille University",
+  },
+  {
+    id: 6,
+    name: "Dr Marieke Meelen",
+    image: "",
+    description: "Director of studies in Linguistics, Cambridge University",
+  },
+  {
+    id: 7,
+    name: "Wangmo Dixey",
+    image: "",
+    description:
+      "Executive Director at Light of Buddhadharma Foundation International",
+  },
+  {
+    id: 8,
+    name: "Dr Markus kuhn",
+    image: "",
+    description:
+      "Assoicate Professor, Dept. of Computer Science, Cambridge University",
+  },
+  {
+    id: 9,
+    name: "Tsering Gellek",
+    image: "",
+    description: "Director of Sarnath International Nyingma Institute",
+  },
+];
