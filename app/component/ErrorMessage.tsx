8a2081c0f63dc12afc72b244b75e707b5f6a6fd4
kunsang <tenkus47@gmail.com>
2023-11-01 11:04:47 +0530

    update error message

--- app/component/ErrorMessage.tsx
+++ app/component/ErrorMessage.tsx
@@ -1,17 +1,23 @@
+import { Link } from "@remix-run/react";
+
 function ErrorMessage({ error }) {
   console.log(error);
   return (
     <>
-      <div className="py-10 m-auto w-[90%] md:w-[80%]">
-        <div className="flex flex-col justify-center items-center gap-10 px-4 md:flex-row">
-          <img
-            src="/assets/about.jpg"
-            alt="monlam"
-            className="w-[80%] md:w-[60%] lg:w-[40%] border-[#dddcdc] rounded-lg shadow-sm hover:scale-105 transition-all duration-500"
-          />
-          <div className="w-[80%] md:w-[60%] lg:w-[40%] text-center">
-            <h4 className="text-2xl mb-4 text-red-500">འདིར་དཀའ་ངལ་འདུག</h4>
-          </div>
+      <div className="relative h-screen flex justify-center items-center py-10 m-auto w-[90%] md:w-[80%]">
+        <img
+          src="/assets/about.jpg"
+          alt="monlam"
+          className="w-[80%] opacity-40 md:w-[60%] lg:w-[40%] border-[#dddcdc] rounded-lg shadow-sm hover:scale-105 transition-all duration-500"
+        />
+        <div className="w-[80%] md:w-[60%] lg:w-[40%] text-center absolute top-[40%] pointer-events-none">
+          <h4 className="mb-40 text-slate-500 text-4xl">འདིར་དཀའ་ངལ་འདུག</h4>
+          <Link
+            to="/"
+            className="text-blue-500 font-bold text-3xl pointer-events-auto"
+          >
+            འདིར་ནོན།
+          </Link>
         </div>
       </div>
     </>
