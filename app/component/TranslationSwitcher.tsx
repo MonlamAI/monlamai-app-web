3f4f262a29787c903b1c1bf7f8704c1915ab9674
TENZIN TAMDIN <tenzintamdin@TENZINs-MacBook-Air.local>
2023-12-15 11:23:34 +0530

    dark mode on translate switch

--- app/component/TranslationSwitcher.tsx
+++ app/component/TranslationSwitcher.tsx
@@ -41,7 +41,7 @@ function TranslationSwitcher() {
       />
       {isOpen && (
         <div
-          className="origin-top-right absolute right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+          className="origin-top-right absolute right-0 mt-2 rounded-md shadow-lg bg-white dark:bg-slate-700 ring-1 ring-black ring-opacity-5"
           role="menu"
           aria-orientation="vertical"
           aria-labelledby={LANGUAGE_SELECTOR_ID}
@@ -54,8 +54,8 @@ function TranslationSwitcher() {
                   onClick={() => handleLanguageChange(language.code)}
                   className={`${
                     selectedLanguage.code === language.code
-                      ? "bg-gray-100 text-gray-900"
-                      : "text-gray-700"
+                      ? "bg-gray-100 text-gray-900 dark:bg-gray-600 dark:text-gray-100"
+                      : "text-gray-700 dark:text-gray-300"
                   }   px-4 py-2 text-sm text-start items-center inline-flex hover:bg-gray-100 ${
                     index % 2 === 0 ? "rounded-r" : "rounded-l"
                   }`}
