28ab90f495e2e5d7115ce8894943d97638c849a7
kunsang <tenkus47@gmail.com>
2024-01-17 14:39:33 +0530

    add card on tools

--- app/routes/_index/component/Tools.tsx
+++ app/routes/_index/component/Tools.tsx
@@ -44,37 +44,6 @@ function EachModel({ model }: EachProps) {
     <NavLink prefetch="intent" to={"/model/" + link} unstable_viewTransition>
       {({ isTransitioning }) => (
         <motion.div whileHover={{ scale: 0.95 }} className="h-full">
-          {/* <div
-            className="flex justify-center dark:mix-blend-exclusion dark:rounded-lg  "
-            style={
-              isTransitioning
-                ? {
-                    viewTransitionName: "icon-transition",
-                  }
-                : undefined
-            }
-          >
-            {icon}
-          </div>
-          <div
-            className={`flex flex-col justify-around flex-1 md:gap-5 text-[1.25rem] md:text-[2.2rem] 
-        ${isEnglish ? "font-poppins" : "font-monlam"}`}
-          >
-            <h2
-              className={`${
-                isEnglish ? "text-[1rem]" : "text-[1.2rem]"
-              } md:text-[1.4rem]`}
-            >
-              {translation[name]}
-            </h2>
-            <p
-              className={`text-gray-400 ${
-                isEnglish ? "text-[0.7rem]" : "text-[0.7rem]"
-              } md:text-[0.7rem]`}
-            >
-              {translation[desc]}
-            </p>
-          </div> */}
           <Card
             className="h-full flex flex-row md:flex-col"
             renderImage={() => (
