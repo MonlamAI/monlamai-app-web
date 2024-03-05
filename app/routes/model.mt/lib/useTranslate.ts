bb921df800c7e1f909b2bbf0f49f969672d08365
tentamdin <tgtamdin3@gmail.com>
2024-03-05 10:48:19 +0530

    fix: mt output spacing

--- app/routes/model.mt/lib/useTranslate.ts
+++ app/routes/model.mt/lib/useTranslate.ts
@@ -4,22 +4,21 @@ import { useState, useEffect } from "react";
 type useTranslateType = {
   target: string;
   text: string;
-  data:string;
+  data: string;
   setData: (data: string) => void;
 };
 
-const useTranslate = ({ target, text,data,setData }: useTranslateType) => {
+const useTranslate = ({ target, text, data, setData }: useTranslateType) => {
   const [done, setDone] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const { url, token } = useLoaderData();
   const controller = new AbortController();
-   function trigger(){
-    
+  function trigger() {
     if (!text) {
       // Avoid fetching if text is empty or not provided
-      setData('');
-      setError('Text is required for translation.');
+      setData("");
+      setError("Text is required for translation.");
       return;
     }
 
@@ -45,7 +44,9 @@ const useTranslate = ({ target, text,data,setData }: useTranslateType) => {
         });
 
         if (!response.ok) {
-          throw new Error(`Network response was not ok, status: ${response.status}`);
+          throw new Error(
+            `Network response was not ok, status: ${response.status}`
+          );
         }
 
         await handleResponse(response);
@@ -58,47 +59,43 @@ const useTranslate = ({ target, text,data,setData }: useTranslateType) => {
     };
 
     fetchData();
-// Effect dependencies
-}
-
-async function handleResponse(response) {
-  setData("")
-  const contentType = response.headers.get("Content-Type");
-  let chunk="";
-  if (contentType && contentType.includes("application/json")) {
-    // Handle JSON response
-    const jsonResponse = await response.json();
-    setData(jsonResponse[0].generated_text);
-  } else if (contentType && contentType.includes("text/event-stream")) {
-    // Handle streaming response using asynchronous iterator
-    const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
-    let streamData = "";
-    while(true){
-
-      const { done, value } = await reader.read();
-      if(done) break;
-      chunk='';
-      chunk = value.replace(/^data:/, "").replace(/\s+/g, '')
-      
-      try{
-        streamData= JSON.parse(chunk).token.text;
-      }catch(e){
-        console.log(e);
-      }
-        
-        setData(p=>
-          {
-
-           return p+streamData.replace('</s>','')
-          });
-    }
-
-   
+    // Effect dependencies
   }
-}
 
+  async function handleResponse(response) {
+    setData("");
+    const contentType = response.headers.get("Content-Type");
+    let chunk = "";
+    if (contentType && contentType.includes("application/json")) {
+      // Handle JSON response
+      const jsonResponse = await response.json();
+      setData(jsonResponse[0].generated_text);
+    } else if (contentType && contentType.includes("text/event-stream")) {
+      // Handle streaming response using asynchronous iterator
+      const reader = response.body
+        .pipeThrough(new TextDecoderStream())
+        .getReader();
+      let streamData = "";
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        chunk = "";
+        chunk = value.replace(/^data:/, "").replace(/^\s/, "");
 
-  return { data, isLoading, error, done,trigger };
+        try {
+          streamData = JSON.parse(chunk).token.text;
+        } catch (e) {
+          console.log(e);
+        }
+
+        setData((p) => {
+          return p + streamData.replace("</s>", "");
+        });
+      }
+    }
+  }
+
+  return { data, isLoading, error, done, trigger };
 };
 
 export default useTranslate;
