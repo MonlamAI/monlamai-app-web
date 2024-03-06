5631eac267e2958e50574ab42eb236b3450d5638
tenkus47 <tenkus47@gmail.com>
2024-03-06 16:59:14 +0530

    updated lang detection

--- app/routes/model.mt/components/LanguageInput.tsx
+++ app/routes/model.mt/components/LanguageInput.tsx
@@ -1,12 +1,21 @@
 import { useSearchParams } from "@remix-run/react";
 import { Select } from "flowbite-react";
-import React from "react";
+import React, { useEffect } from "react";
 import { FaArrowRightArrowLeft } from "react-icons/fa6";
 import { resetFetcher } from "~/component/utils/resetFetcher";
+import LanguageDetect from "languagedetect";
 
-function LanguageInput({ likefetcher, setSourceText, data, setTranslated }) {
+const lngDetector = new LanguageDetect();
+
+function LanguageInput({
+  likefetcher,
+  sourceText,
+  setSourceText,
+  data,
+  setTranslated,
+}) {
   const [params, setParams] = useSearchParams();
-  const sourceLang = params.get("source") || "en";
+  const sourceLang = params.get("source") || "detect language";
   const targetLang = params.get("target") || "bo";
   function handleChangeTo(event: React.ChangeEvent<HTMLSelectElement>) {
     const lang = event.target.value;
@@ -34,9 +43,26 @@ function LanguageInput({ likefetcher, setSourceText, data, setTranslated }) {
       return p;
     });
   }
+  useEffect(() => {
+    if (sourceLang !== "detect language") return;
+    let lang = lngDetector.detect(sourceText);
+    let ranked = lang.map((l) => l[0]);
+    let option = languagesOptions.map((l) => l.value.toLowerCase());
+    let common = findFirstCommonElement(ranked, option);
+    if (common) {
+      let newLang = languagesOptions.find(
+        (l) => l.value.toLowerCase() === common
+      );
+      setParams((p) => {
+        p.set("source", newLang?.code);
+        return p;
+      });
+    }
+  }, [sourceText]);
   return (
     <div className="flex items-center justify-center md:flex-row gap-3 mt-2 ">
       <Select onChange={handleChangefrom} value={sourceLang}>
+        <option>detect language</option>
         {languagesOptions.map((lang) => (
           <option key={lang.code} value={lang.code}>
             {lang.value}
@@ -85,3 +111,12 @@ const languagesOptions = [
   { value: "Polish", code: "pl" },
   { value: "Greek", code: "el" },
 ];
+
+function findFirstCommonElement(array1, array2) {
+  for (let element of array1) {
+    if (array2.includes(element)) {
+      return element;
+    }
+  }
+  return undefined; // Or return null, or any other value to indicate no common elements were found.
+}
