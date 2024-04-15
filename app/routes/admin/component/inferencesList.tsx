aa5480c195cddd66af4daaddff7ed9d7581c6edf
tenkus47 <tenkus47@gmail.com>
2024-04-15 15:17:14 +0530

    restrict ocr data

--- app/routes/admin/component/inferencesList.tsx
+++ app/routes/admin/component/inferencesList.tsx
@@ -48,7 +48,9 @@ const InferenceList = ({ inferences }) => {
       ],
     });
 
-    const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+    const blob = new Blob(["\uFEFF" + csv], {
+      type: "text/csv;charset=utf-8;",
+    });
     const url = URL.createObjectURL(blob);
     const link = document.createElement("a");
     link.setAttribute("href", url);
