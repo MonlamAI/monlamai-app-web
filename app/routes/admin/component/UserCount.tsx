adc3f2435cf921e9a4f27f0936b909109c66cc4c
tenkus47 <tenkus47@gmail.com>
2024-05-27 11:14:47 +0530

    update

--- app/routes/admin/component/UserCount.tsx
+++ app/routes/admin/component/UserCount.tsx
@@ -4,7 +4,14 @@ import React from "react";
 function UserCount() {
   let { usercount } = useLoaderData();
 
-  return <div>total user: {usercount}</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-white shadow-md rounded-lg p-6">
+        <h1 className="text-2xl font-semibold text-gray-800">Total Users</h1>
+        <p className="mt-4 text-lg text-gray-600">{usercount}</p>
+      </div>
+    </div>
+  );
 }
 
 export default UserCount;
