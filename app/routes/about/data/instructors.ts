7f02de8d434e374b7a05ead2e520e125f8bc0b84
tenkus47 <tenkus47@gmail.com>
2024-06-07 13:04:40 +0530

    updated lama and instructors

--- app/routes/about/data/instructors.ts
+++ app/routes/about/data/instructors.ts
@@ -2,35 +2,35 @@ export const instructors = [
   {
     id: 1,
     name: "DR. KURT KEUTZER",
-    tibetan_name: "གུར་ཀུཏ་ཀེར་ཊི་ཀོབ་རི།",
+    tibetan_name: "དགེ་རྒན་ཆེན་མོ་ཀཱར་ཊི་ཀུ་ཟར།",
     image: "/assets/instructors/kurt.png",
     description: "Professor Emeritus, UC Berkely",
   },
   {
     id: 2,
     name: "Geshe Thupten Jinpa, Phd",
-    tibetan_name: "དགེ་ཤེས་ཐུབ་བསྟན་འཇིན་པ་ཕོ།",
+    tibetan_name: "ལྷ་རམས་པ་དགེ་བཤེས་ཐུབ་བསྟན་སྦྱིན་པ།",
     image: "/assets/instructors/thunpten.png",
     description: "Founder, Compassion Institute Various",
   },
   {
     id: 3,
     name: "Geshe Dorji Damdul",
-    tibetan_name: "དགེ་ཤེས་རྡོ་རྗེ་དམག་དུལ།",
+    tibetan_name: "དགེ་བཤེས་རྡོ་རྗེ་དགྲ་འདུལ།",
     image: "/assets/instructors/dorji.jpeg",
     description: "Director, Tibet House, New Delhi",
   },
   {
     id: 4,
     name: "Sebastian Nehrdich",
-    tibetan_name: "སེ་བའི་སྣེ་རྡི་ཆ།",
+    tibetan_name: "སེ་བྷའེ་ཤན། ནཱེར་ཌི་ཆི།",
     image: "/assets/instructors/sebastian.jpeg",
     description: "Researcher, UC Berkeley",
   },
   {
     id: 5,
     name: "Nicolas Tournadre",
-    tibetan_name: "ནི་ཀོ་ལས་ཏོར་ནད་རི།",
+    tibetan_name: "ཉི་སྐུ་བདེ་ཆེན་རབ་བརྟན།",
     image: "/assets/instructors/Nicolas.png",
     description: "Professor, Dept. of linguistics, Aix-Marseille University",
   },
@@ -44,7 +44,7 @@ export const instructors = [
   {
     id: 7,
     name: "Wangmo Dixey",
-    tibetan_name: "དབང་མོ་དི་ཤེ།",
+    tibetan_name: "དབང་མོ།",
     image: "/assets/instructors/wangmo.png",
     description:
       "Executive Director at Light of Buddhadharma Foundation International",
@@ -52,7 +52,7 @@ export const instructors = [
   {
     id: 8,
     name: "Dr Markus kuhn",
-    tibetan_name: "མ་ཀ་རུས་ཁུན།",
+    tibetan_name: "མཱར་ཀུ་སི་ཁོན།",
     image: "/assets/instructors/markus.jpeg",
     description:
       "Assoicate Professor, Dept. of Computer Science, Cambridge University",
@@ -60,7 +60,7 @@ export const instructors = [
   {
     id: 9,
     name: "Tsering Gellek",
-    tibetan_name: "ཚེ་རིང་གེ་ལེཀ།",
+    tibetan_name: "ཚེ་རིང་དགེ་ལེགས།",
     image: "/assets/instructors/tsering.png",
     description: "Director of Sarnath International Nyingma Institute",
   },
