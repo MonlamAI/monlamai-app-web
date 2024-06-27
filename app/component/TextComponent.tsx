2da89c53d34ee4dae181e60cdbf8e13fa32cda4d
tenkus47 <tenkus47@gmail.com>
2024-06-27 11:14:37 +0530

    update textarea to div contenteditable

--- app/component/TextComponent.tsx
+++ app/component/TextComponent.tsx
@@ -1,49 +1,20 @@
-import { Textarea } from "flowbite-react";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useCallback } from "react";
 import uselitteraTranlation from "~/component/hooks/useLitteraTranslation";
+import sanitizeHtml from "sanitize-html";
 
 function TextComponent({ sourceText, setSourceText, sourceLang }) {
   let { translation, isEnglish } = uselitteraTranlation();
-  let textAreaRef = useRef<HTMLTextAreaElement>(null);
+  let textAreaRef = useRef(null);
   let isEng = sourceLang === "en";
   let isTib = sourceLang === "bo";
-  let prevTextRef = useRef(sourceText);
+
   useEffect(() => {
-    const textRf = document.getElementById("textAreaInput");
-
-    function autoResize() {
-      // This condition ensures that we do not resize past a maximum height
-      if (this.scrollHeight) {
-        this.style.minHeight = "auto"; // Reset minHeight
-        this.style.minHeight = this.scrollHeight + "px"; // Set minHeight to scrollHeight
-      }
-      // window?.scrollTo(0, document.body.scrollHeight);
-    }
-
-    textRf?.addEventListener("input", autoResize, false);
-
-    // Initial call to ensure proper sizing from the start
-    autoResize.call(textRf);
-
-    return () => {
-      textRf?.removeEventListener("input", autoResize);
-    };
+    textAreaRef.current?.addEventListener("paste", function (e) {
+      e.preventDefault();
+      var text = e.clipboardData.getData("text/plain");
+      document.execCommand("insertText", false, text);
+    });
   }, []);
-
-  useEffect(() => {
-    const textRf = document.getElementById("textAreaInput");
-
-    if (
-      sourceText.length > prevTextRef.current.length &&
-      sourceText.startsWith(prevTextRef.current)
-    ) {
-      window.scrollTo(0, document.body.scrollHeight);
-    }
-    if (textRf && (sourceText === "" || sourceText === null)) {
-      textRf.style.minHeight = "auto"; // Reset height
-    }
-    prevTextRef.current = sourceText;
-  }, [sourceText]);
 
   let fontSize =
     sourceText.length < 600
@@ -52,23 +23,30 @@ function TextComponent({ sourceText, setSourceText, sourceLang }) {
       ? "text-base"
       : "text-sm";
 
+  const onContentBlur = useCallback((evt) => {
+    const sanitizeConf = {
+      allowedTags: ["b", "i", "a", "p"],
+      allowedAttributes: { a: ["href"] },
+    };
+    let html = sanitizeHtml(evt.target.innerText, sanitizeConf);
+    setSourceText(html);
+  }, []);
+
   return (
-    <Textarea
+    <div
       id="textAreaInput"
       name="sourceText"
-      placeholder={translation.inputPlaceholder}
-      className={`${
+      className={`p-2 ${
         isEnglish ? "placeholder:font-poppins" : "placeholder:font-monlam"
       } w-full rounded-none resize-none flex-1 bg-transparent border-0 dark:border:0 focus:outline-none dark:focus:outline-none focus:ring-transparent dark:focus:ring-transparent caret-slate-500 placeholder:text-slate-300 placeholder:font-monlam placeholder:text-lg
        ${fontSize} ${isEng && "font-poppins  "} ${
         isTib && "leading-loose font-monlam "
       } ${!isEng && !isTib && "font-notosans "}`}
+      placeholder={translation.inputPlaceholder}
+      contentEditable
       required
-      value={sourceText}
-      onInput={(e) => {
-        let value = e.target?.value;
-        setSourceText(value);
-      }}
+      onInput={onContentBlur}
+      onBlur={onContentBlur}
       autoFocus
       ref={textAreaRef}
     />
