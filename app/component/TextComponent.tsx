bd239f911f3319bc4309a086d6ed40dedb678e76
tenkus47 <tenkus47@gmail.com>
2024-07-05 15:11:59 +0530

    update text componet

--- app/component/TextComponent.tsx
+++ app/component/TextComponent.tsx
@@ -26,15 +26,8 @@ function TextComponent({ sourceText, setSourceText, sourceLang }) {
   useEffect(() => {
     if (offset !== undefined) {
       const newRange = document.createRange();
-      let text = textRef.current?.childNodes[0];
-      if (
-        textRef.current.childNodes[0] === undefined ||
-        textRef.current.childNodes[0] === null
-      ) {
-        text = document.createTextNode(""); // Create an empty text node
-        textRef.current.appendChild(text);
-      }
-      newRange?.setStart(text, offset);
+      if (textRef.current.childNodes[0] === undefined) return;
+      newRange?.setStart(textRef.current.childNodes[0], offset);
       const selection = document.getSelection();
       selection?.removeAllRanges();
       selection?.addRange(newRange);
@@ -48,36 +41,47 @@ function TextComponent({ sourceText, setSourceText, sourceLang }) {
       ? "text-base"
       : "text-sm";
 
-  const onContentBlur = useCallback((evt) => {
+  const onInput = useCallback((evt) => {
+    const selection = window.getSelection();
+    console.log("selection", evt.target.innerHTML);
     const sanitizeConf = {
       allowedTags: ["b", "i", "a", "p"],
       allowedAttributes: { a: ["href"] },
     };
-    let html = sanitizeHtml(evt.target.innerText, sanitizeConf);
-
-    const range = document.getSelection().getRangeAt(0);
-    setOffset(range.startOffset);
-    setSourceText(html);
+    const html = sanitizeHtml(evt.target.innerHTML, sanitizeConf);
+    if (selection.rangeCount > 0) {
+      const range = selection.getRangeAt(0);
+      setOffset(range.startOffset);
+      setSourceText(html);
+    }
   }, []);
-
   return (
-    <div
-      id="textAreaInput"
-      className={`p-2 pr-6 ${
-        isEnglish ? "placeholder:font-poppins" : "placeholder:font-monlam"
-      } w-full rounded-none resize-none flex-1 bg-transparent border-0 dark:border:0 focus:outline-none dark:focus:outline-none focus:ring-transparent dark:focus:ring-transparent caret-slate-500 placeholder:text-slate-300 placeholder:font-monlam placeholder:text-lg
+    <>
+      <div
+        id="textAreaInput"
+        name="sourceText"
+        className={`p-2 pr-6 w-full rounded-none resize-none flex-1 bg-transparent border-0 dark:border:0 focus:outline-none dark:focus:outline-none focus:ring-transparent dark:focus:ring-transparent caret-slate-500 placeholder:text-slate-300 placeholder:font-monlam placeholder:text-lg
        ${fontSize} ${isEng && "font-poppins  "} ${
-        isTib && "leading-loose font-monlam "
-      } ${!isEng && !isTib && "font-notosans "}`}
-      placeholder={translation.inputPlaceholder}
-      contentEditable
-      required
-      onInput={onContentBlur}
-      autoFocus
-      ref={textRef}
-      suppressContentEditableWarning
-      dangerouslySetInnerHTML={{ __html: sourceText }}
-    />
+          isTib && "leading-loose font-monlam "
+        } ${!isEng && !isTib && "font-notosans "}`}
+        contentEditable
+        required
+        onInput={onInput}
+        autoFocus
+        ref={textRef}
+        suppressContentEditableWarning
+        dangerouslySetInnerHTML={{ __html: sourceText }}
+      />
+      {sourceText.length === 0 && (
+        <span
+          className={`absolute p-3 inset-0 text-gray-500 pointer-events-none ${
+            isEnglish ? "font-poppins" : "font-monlam"
+          }`}
+        >
+          {translation.inputPlaceholder}
+        </span>
+      )}
+    </>
   );
 }
 
