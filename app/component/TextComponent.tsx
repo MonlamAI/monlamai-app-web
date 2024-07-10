9de846b8122e553eee9d7090a284a96a25d81e9d
tenkus47 <tenkus47@gmail.com>
2024-07-10 10:59:11 +0530

    fixed text area

--- app/component/TextComponent.tsx
+++ app/component/TextComponent.tsx
@@ -1,61 +1,14 @@
-import {
-  useEffect,
-  useRef,
-  useCallback,
-  useState,
-  useLayoutEffect,
-} from "react";
+import { useEffect, useRef, useCallback, useState } from "react";
 import uselitteraTranlation from "~/component/hooks/useLitteraTranslation";
+import ContentEditable from "react-contenteditable";
 import sanitizeHtml from "sanitize-html";
 
 function TextComponent({ sourceText, setSourceText, sourceLang }) {
   let { translation, isEnglish } = uselitteraTranlation();
+  let [html, setHTML] = useState("");
   const textRef = useRef(null);
-  const caretPos = useRef(0);
   let isEng = sourceLang === "en";
   let isTib = sourceLang === "bo";
-
-  function getCaret(el) {
-    let caretAt = 0;
-    const sel = window.getSelection();
-
-    if (sel.rangeCount === 0) {
-      return caretAt;
-    }
-
-    const range = sel.getRangeAt(0);
-    const preRange = range.cloneRange();
-    preRange.selectNodeContents(el);
-    preRange.setEnd(range.endContainer, range.endOffset);
-    caretAt = preRange.toString().length;
-
-    return caretAt;
-  }
-
-  function setCaret(el, offset) {
-    const sel = window.getSelection();
-    const range = document.createRange();
-
-    range.setStart(el?.childNodes[0], offset);
-    range.collapse(true);
-    sel.removeAllRanges();
-    sel.addRange(range);
-  }
-
-  useEffect(() => {
-    if (sourceText && textRef.current && caretPos.current !== 0) {
-      setCaret(textRef.current, caretPos.current);
-      textRef.current.focus();
-    }
-  }, [sourceText]);
-
-  useEffect(() => {
-    textRef.current?.addEventListener("paste", function (e) {
-      e.preventDefault();
-      var text = e.clipboardData.getData("text/plain");
-      document.execCommand("insertText", false, text);
-    });
-  }, []);
 
   let fontSize =
     sourceText.length < 600
@@ -64,40 +17,34 @@ function TextComponent({ sourceText, setSourceText, sourceLang }) {
       ? "text-base"
       : "text-sm";
 
-  const onInput = useCallback(
-    (evt) => {
-      caretPos.current = getCaret(textRef.current);
-      const sanitizeConf = {
-        allowedTags: ["b", "i", "a", "p"],
-        allowedAttributes: { a: ["href"] },
-      };
-      const html = sanitizeHtml(evt.target.innerHTML, sanitizeConf);
-      setSourceText(html);
-
-      // After setting the source text, ensure the cursor is at the correct position
-      const newCaretPos = getCaret(textRef.current);
-      if (caretPos.current !== newCaretPos) {
-        setCaret(textRef.current, caretPos.current);
-      }
-    },
-    [setSourceText]
-  );
+  const handleChange = (evt) => {
+    let html = evt.target.value;
+    setHTML(html);
+    const sanitizedHtml = sanitizeHtml(html, {
+      allowedTags: ["div"],
+      allowedIframeHostnames: ["www.youtube.com"],
+    });
+    const cleanText = sanitizedHtml
+      .replace(/<div>/g, "\n")
+      .replace(/<\/div>/g, "");
+    setSourceText(cleanText);
+    console.log(cleanText);
+  };
   return (
     <>
-      <div
-        id="textAreaInput"
+      <ContentEditable
+        innerRef={textRef}
+        html={html} // innerHTML of the editable div
+        disabled={false} // use true to disable editing
+        onChange={handleChange} // handle innerHTML change
+        tagName="article" // Use a custom HTML tag (uses a div by default)
         className={`p-2 pr-6 w-full rounded-none resize-none flex-1 bg-transparent border-0 dark:border:0 focus:outline-none dark:focus:outline-none focus:ring-transparent dark:focus:ring-transparent caret-slate-500 placeholder:text-slate-300 placeholder:font-monlam placeholder:text-lg
-       ${fontSize} ${isEng && "font-poppins  "} ${
+          ${fontSize} ${isEng && "font-poppins  "} ${
           isTib && "leading-loose font-monlam "
         } ${!isEng && !isTib && "font-notosans "}`}
-        contentEditable
-        required
-        onInput={onInput}
-        autoFocus
-        ref={textRef}
-        suppressContentEditableWarning
-        dangerouslySetInnerHTML={{ __html: sourceText }}
+        autofocus={true}
       />
+
       {sourceText.length === 0 && (
         <span
           className={`absolute p-3 inset-0 text-gray-500 pointer-events-none ${
