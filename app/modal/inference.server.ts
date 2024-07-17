3c963508cdbbd4c52ee0ba62167173b2f9aaf38b
tenkus47 <tenkus47@gmail.com>
2024-07-17 18:01:08 +0530

    getUserFileInfereces to return empty array

--- app/modal/inference.server.ts
+++ app/modal/inference.server.ts
@@ -85,32 +85,33 @@ export async function addFileInference({
 }
 
 export async function getUserFileInferences({ userId, model }) {
-  let OR = undefined;
-  if (model === "ocr") {
-    OR = [
-      {
-        input: { endsWith: ".zip" },
-      },
-      {
-        input: { endsWith: ".gz" },
-      },
-      {
-        input: { endsWith: ".pdf" },
-      },
-    ];
-  }
-  let result = await db.inference.findMany({
-    where: {
-      userId: typeof userId === "string" ? parseInt(userId) : userId,
-      model,
-      OR,
-    },
-    orderBy: {
-      updatedAt: "desc",
-    },
-    take: 20,
-  });
-  return result;
+  return [];
+  // let OR = undefined;
+  // if (model === "ocr") {
+  //   OR = [
+  //     {
+  //       input: { endsWith: ".zip" },
+  //     },
+  //     {
+  //       input: { endsWith: ".gz" },
+  //     },
+  //     {
+  //       input: { endsWith: ".pdf" },
+  //     },
+  //   ];
+  // }
+  // let result = await db.inference.findMany({
+  //   where: {
+  //     userId: typeof userId === "string" ? parseInt(userId) : userId,
+  //     model,
+  //     OR,
+  //   },
+  //   orderBy: {
+  //     updatedAt: "desc",
+  //   },
+  //   take: 20,
+  // });
+  // return result;
 }
 
 export async function deleteInference({ id }) {
