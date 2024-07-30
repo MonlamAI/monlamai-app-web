8255405b02212aed061579bbc4c179d621e975d3
tenkus47 <tenkus47@gmail.com>
2024-07-30 22:19:38 +0530

    update morgan

--- server.js
+++ server.js
@@ -120,7 +120,14 @@ app.use(
   "/build",
   express.static("public/build", { immutable: true, maxAge: "1y" })
 );
-app.use(morgan("tiny"));
+morgan.token("ip", (req) => req.ip);
+app.use(
+  morgan("-:method :ip :url :status :response-time ms", {
+    stream: {
+      write: (message) => console.log(message.trim()),
+    },
+  })
+);
 
 app.all(
   "*",
