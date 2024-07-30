9cdc0d0fc8ca88f70afa60953ec944f4b4a6af89
tenkus47 <tenkus47@gmail.com>
2024-07-30 11:47:27 +0530

    update metatext logging

--- app/root.tsx
+++ app/root.tsx
@@ -171,6 +171,30 @@ function Document({ children }: { children: React.ReactNode }) {
         <meta name="apple-mobile-web-app-title" content="Monlam Chat" />
         <Meta />
         <Links />
+        <script
+          key="stloader"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function(e,r,n,t,s){var a=[];e[s]=function(){a.push(arguments)};e[s].queue=a;  var o=[];var i=[];var c=true;var p=void 0;if(window.PerformanceObserver&&  window.PerformanceObserver.supportedEntryTypes&&(  PerformanceObserver.supportedEntryTypes.indexOf("longtask")>=0||  PerformanceObserver.supportedEntryTypes.indexOf("element")>=0)){  p=new PerformanceObserver(function(e){e.getEntries().forEach(function(e){  switch(e.entryType){case"element":i.push(e);break;case"longtask":o.push(e);break;  default:break}})});p.observe({entryTypes:["longtask","element"]})}e[s+"lt"]={  longTasks:o,timingElements:i,inPageLoad:c,observer:p};if(t){var u=r.createElement(n);  u.async=1;u.src=t;var f=r.getElementsByTagName(n)[0];f.parentNode.insertBefore(u,f)}})
+    (window,document,"script","//cdn.sematext.com/experience.js","strum");
+  `,
+          }}
+        />
+        <script
+          key="stconfig"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+    strum('config', { token: '521e1ed7-963f-4496-a178-bcd589c08b4e', 'receiverUrl': 'https://rum-receiver.eu.sematext.com' });
+    var oldPushState = history.pushState;
+    history.pushState = function(state, title, url) {
+      window['strum']('routeChange', url);
+      return oldPushState.apply(history, arguments);
+    };
+ `,
+          }}
+        />
       </head>
       <SocketProvider socket={socket}>
         <body className="flex h-[100dvh]  mx-auto inset-0 overflow-y-auto overflow-x-hidden dark:bg-slate-700 dark:text-gray-200">
