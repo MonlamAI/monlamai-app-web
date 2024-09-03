3fbe4d12c873e4f286ebfa0006128fea31af0532
tentamdin <tgtamdin3@gmail.com>
2024-09-03 13:07:40 +0530

    remove random sort and corrected spelling

--- app/routes/team/route.tsx
+++ app/routes/team/route.tsx
@@ -17,9 +17,7 @@ function Team() {
   let { translation, isTibetan } = uselitteraTranlation();
 
   const teamData = isTibetan ? TibetanTeam : EnglishTeam;
-  let sortedTeamData = useMemo(
-    () => teamData.sort(() => Math.random() - 0.5)
-  ,[])
+
   return (
     <div
       className={`m-auto w-[90%] md:w-[80%]  flex flex-col ${
@@ -29,19 +27,20 @@ function Team() {
       <ToolWraper title="team">
         <div className="flex flex-col md:flex-row mb-8 justify-between items-center">
           <div className="w-full md:w-1/2 mb-4 text-left">
-            <h1 className="text-4xl font-bold mb-4">Our Team</h1>
+            <h1 className="text-4xl font-bold mb-4">
+              {isTibetan ? "ང་ཚོའི་རུ་ཁག།" : "Our Team"}
+            </h1>
             <p>
-              {isTibetan?`
-              མོན་ལམ་བཟོ་ལས་རིག་པའི་རུ་ཁག་གི་ཁོངས་མི་རེ་རེས་གྲངས་ཉུང་མི་རིགས་ཀྱི་སྐད་ཡིག་དང་རིག་གཞུང་གི་ཆེད་དུ་འཕྲུལ་རིག་གིས་ཅི་ཞིག་བྱེད་ཐུབ་པའི་ས་མཚམས་རྒྱ་བསྐྱེད་གཏོང་རྒྱུར་ཧུར་བརྩོན་བྱེད་བཞིན་ཡོད། ང་ཚོའི་རྒྱབ་ལྗོངས་ནི་ང་ཚོའི་ལས་འགན་ལྟར་སྣ་མང་ཡིན་ཡང་། ང་ཚོ་ནི་དོན་སྙིང་ལྡན་པའི་ཤུགས་རྐྱེན་ཞིག་ཐེབས་ཐབས་བྱ་རྒྱུའི་ཆོད་སེམས་ཀྱིས་མཉམ་དུ་འབྲེལ་ཡོད། མཉེན་ཆས་བཟོ་སྐྲུན་པ་ནས་བོད་སྐད་ཆེད་མཁས་པ་དང་རིག་གཞུང་སློབ་སྟོན་པའི་བར། ང་ཚོའི་རུ་ཁག་གིས་ང་ཚོའི་ལས་འཆར་རྣམས་བོད་མིའི་སྤྱི་ཚོགས་ཀྱི་དགོས་མཁོ་དང་རེ་བ་དང་མཐུན་པ་ཡོང་བར་ཉེ་བར་མཉམ་ལས་བྱེད་བཞིན་ཡོད།`:
-              `Our Team Each member of the Monlam AI team is dedicated to pushing
+              {isTibetan
+                ? `
+              མོན་ལམ་བཟོ་ལས་རིག་པའི་རུ་ཁག་གི་ཁོངས་མི་རེ་རེས་གྲངས་ཉུང་མི་རིགས་ཀྱི་སྐད་ཡིག་དང་རིག་གཞུང་གི་ཆེད་དུ་འཕྲུལ་རིག་གིས་ཅི་ཞིག་བྱེད་ཐུབ་པའི་ས་མཚམས་རྒྱ་བསྐྱེད་གཏོང་རྒྱུར་ཧུར་བརྩོན་བྱེད་བཞིན་ཡོད། ང་ཚོའི་རྒྱབ་ལྗོངས་ནི་ང་ཚོའི་ལས་འགན་ལྟར་སྣ་མང་ཡིན་ཡང་། ང་ཚོ་ནི་དོན་སྙིང་ལྡན་པའི་ཤུགས་རྐྱེན་ཞིག་ཐེབས་ཐབས་བྱ་རྒྱུའི་ཆོད་སེམས་ཀྱིས་མཉམ་དུ་འབྲེལ་ཡོད། མཉེན་ཆས་བཟོ་སྐྲུན་པ་ནས་བོད་སྐད་ཆེད་མཁས་པ་དང་རིག་གཞུང་སློབ་སྟོན་པའི་བར། ང་ཚོའི་རུ་ཁག་གིས་ང་ཚོའི་ལས་འཆར་རྣམས་བོད་མིའི་སྤྱི་ཚོགས་ཀྱི་དགོས་མཁོ་དང་རེ་བ་དང་མཐུན་པ་ཡོང་བར་ཉེ་བར་མཉམ་ལས་བྱེད་བཞིན་ཡོད།`
+                : `Each member of the Monlam AI team is dedicated to pushing
               the boundaries of what technology can do for minority languages
               and cultures. Our backgrounds are as diverse as our roles, yet we
               are united by a commitment to making a meaningful impact. From
               software engineers to Tibetan language experts and cultural
               advisors, our team collaborates closely to ensure that our
-              projects align with the needs and aspirations of the Tibetan community.`
-              }
-              
+              projects align with the needs and aspirations of the Tibetan community.`}
             </p>
           </div>
           <div className="w-full md:w-1/3">
@@ -53,13 +52,12 @@ function Team() {
           </div>
         </div>
         <h2 className="text-2xl font-semibold mb-6 text-center">
-          {isTibetan?"ང་ཚོའི་རུ་ཁག་དང་མཇལ་འཕྲད་གནང་རོགས།":"Meet Our Team"}
+          {isTibetan ? "ང་ཚོའི་རུ་ཁག་དང་མཇལ་འཕྲད་གནང་རོགས།" : "Meet Our Team"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-16">
-          {sortedTeamData
-            .map((member, index) => (
-              <TeamMember key={index} {...member} />
-            ))}
+          {teamData.map((member, index) => (
+            <TeamMember key={index} {...member} />
+          ))}
           <Outlet />
         </div>
       </ToolWraper>
